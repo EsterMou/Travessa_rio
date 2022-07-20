2c7b3693fad1230df9d8b947a183311304cfba43
EsterMou <mouraester1998@gmail.com>
2022-07-19 22:16:11 -0300

    commit
    correções de bug

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-let hora = 0;
-let minuto = 0;
-let segundo = 0;
+const seg = document.querySelector('.segundo')
+const min = document.querySelector('.minuto')
+const miliseg = document.querySelector('.milisegundo')
 
-let cron;
+let miliNum = 0
+let segNum = 0
+let minNum = 0
+let INTERVALO
 
-document.form_cronometro.iniciar.onclick = () => iniciar();
-document.form_cronometro.reiniciar.onclick = () => reiniciar();
-
-function iniciar(){
-    clearInterval(cron);
-    cron = setInterval(() => {timer(); }, 10);
+function milissegundo(){
+    miliNum++
+    miliseg.innerHTML = miliNum
 }
-
-function reiniciar(){
-    hora = 0;
-    minuto = 0;
-    segundo = 0; 
-
-    document.getElementById('hora').innerText = "00";
-    document.getElementById('minuto').innerText = "00";
-    document.getElementById('segundo').innerText = "00";
+function segundos(){
+    segNum++
+    seg.innerHTML = segNum
+}
+function minutos(){
+    minNum++
+    min.innerHTML = minNum
+}
+function iniciar(){
+    clearInterval(INTERVALO)
+    INTERVALO = setInterval(() => {
+        milissegundo()
+    }, 10)
 }
